471403ceb4199d8011e939f550603eceeeea12ac
Przemyslaw Prencel <przemyslaw.prencel@goodylabs.com>
2019-01-09 16:24:45 +0100

    singleton

--- src/main.kt
+++ src/main.kt
@@ -1,15 +1,10 @@
 fun main(args : Array<String>) {
+    println(Liblary.table);
+    println(Liblary.bookstand);
+}
 
-    val w1 = Print().getPrint("Adam Mickiewicz")
-    /* W zależności od autora wybiera się odpowiednie wydawnictwo. Wpisać kilka wariantów dla różnych nazwisk. Tu powstanie wydawnictwo poematów */
-    val k1 = w1?.createBook("Dziady", 130)
-    /* Tworzy książkę klasy Poemat z podanym tytułem i liczbą stron.  Autor przekazany będzie z wydawnictwa */
-    println(k1)
-
-    val w2 = Print().getPrint("Jan Kochanowski")
-    /* W zależności od autora wybiera się odpowiednie wydawnictwo. Wpisać kilka wariantów dla różnych nazwisk. Tu powstanie wydawnictwo poematów */
-    val k2 = w2?.createBook("Tren XI", 33)
-    /* Tworzy książkę klasy Poemat z podanym tytułem i liczbą stron.  Autor przekazany będzie z wydawnictwa */
-    println(k2)
+object Liblary { // Singleton
+    val table = Table();
+    val bookstand = Bookstand();
 }
 
