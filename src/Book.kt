80cc7fe75af0395eb6d7def822870009b8163afb
Przemyslaw Prencel <przemyslaw.prencel@goodylabs.com>
2019-01-14 21:04:58 +0100

    secord part made abstract factory

--- src/Book.kt
+++ src/Book.kt
@@ -1,32 +1,14 @@
-interface Genre {
-    val genre: String
-    val author: String
-    fun createBook(title: String, number: Number): Book
-}
-
-open class Book(): Genre{
-    override val author: String
-        get() {
-            return author
-        }
-    override val genre: String
-        get() {
-            return genre
-        }
-    override fun createBook(title: String, pages: Number): Book {
-        this.title = title
-        this.pages = pages
-        return this
-    }
-
-    var title: String = "Zeszyt"
-    var pages: Number? = 64
+open class Book() {
+    open var author: String = ""
+    open var genre: String = ""
+    open var title: String = "Zeszyt"
+    open var pages: Int? = 64
 
     override fun toString(): String {
         return "| $author | $title | $genre | $pages |"
     }
 }
 
-class HistoryNovel(override val genre: String, override val author: String) : Book()
-class MedicalThriller(override val genre: String, override val author: String) : Book()
-class Poem(override val genre: String = "Poem", override val author: String) : Book()
+class HistoryNovel(override var genre: String, override var author: String, override var title: String, override var pages: Int?) : Book()
+class MedicalThriller(override var genre: String, override var author: String, override var title: String, override var pages: Int?) : Book()
+class Poem(override var genre: String, override var author: String, override var title: String, override var pages: Int?) : Book()
--- src/BookFactory.kt
+++ src/BookFactory.kt
@@ -1,10 +1,31 @@
-class Print {
-    fun getPrint(author: String): Genre? {
-        return when (author) {
-            "Adam Mickiewicz", "Henryk Sienkiewicz" -> HistoryNovel("Novel", author)
-            "Stefan Zeromski"          -> MedicalThriller("Thriller", author)
-            "Jan Kochanowski", "Wojciech Baran"          -> Poem("Poem", author)
-            else                          -> null
+abstract class Print {
+    abstract val author: String
+    abstract val genre: String
+    abstract fun createBook(title: String, pages: Int): Book
+
+    companion object {
+        inline fun getPrint(author: String): Print = when (author) {
+            "Adam Mickiewicz", "Henryk Sienkiewicz" -> PrintHistoryNovel("Novel", author)
+            "Stefan Zeromski"    -> PrintMedicalThriller("Thriller", author)
+            "Jan Kochanowski"    -> PrintPoem("Poem", author)
+            else                 -> throw IllegalArgumentException()
         }
+    }
+}
+
+class PrintHistoryNovel(override val genre: String, override val author: String) : Print() {
+    override fun createBook(title: String, pages: Int): Book {
+        return HistoryNovel(this.genre, this.author, title, pages)
+    }
+}
+
+class PrintMedicalThriller(override val genre: String, override val author: String): Print() {
+    override fun createBook(title: String, pages: Int): Book {
+        return MedicalThriller(this.genre, this.author, title, pages)
+    }
+}
+class PrintPoem(override val genre: String, override val author: String): Print(){
+    override fun createBook(title: String, pages: Int): Book {
+        return Poem(this.genre, this.author, title, pages)
     }
 }
